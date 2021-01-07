68fcae1a19ae0e9c41b335e03926a41b000ea6ac
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:57:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,12 @@
 # require modules here
 require 'yaml'
+<<<<<<< HEAD
 def load_library(file)
+=======
+require 'pry'
+def load_library(file)
+  # code goes here
+>>>>>>> b1903fd2a6794db8a44f021dc18fd2368c99221e
   
   library = YAML.load_file(file)
   
@@ -8,6 +14,7 @@ def load_library(file)
     
     if emote_hash.include?(key) == false
       emote_hash[key] = {}
+<<<<<<< HEAD
       emote_hash[key][:english] = {}
       emote_hash[key][:japanese] = {}
     end
@@ -17,6 +24,21 @@ def load_library(file)
   
   end
 
+=======
+    end
+    
+    if emote_hash[key].include?(:english) == false
+      emote_hash[key][english] = {}
+      emote_hash[key][japanese] = {}
+    end
+    
+    emote_hash[key][english] = library[key][0]
+    emote_hash[key][japanese] = library[key][1]
+  end
+  
+  return emote_hash
+  
+>>>>>>> b1903fd2a6794db8a44f021dc18fd2368c99221e
 end
 
 def get_japanese_emoticon(file, emote)
